108891dc8a9f5992dd6271970b39b0307645bc15
Kevin Hookumchand <khookumchand@gmail.com>
2022-03-13 14:23:59 -0400

    Added translation text

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,6 @@ module RossiStyles
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.available_locales = [:en, :fr, :it]
-    config.i18n.default_locales = :en
+    config.i18n.default_locale = :en
   end
 end
