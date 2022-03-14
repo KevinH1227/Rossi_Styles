8422ac5210ffb4c46a9f810061a8e8534f7e03e5
Kevin Hookumchand <khookumchand@gmail.com>
2022-03-13 22:06:56 -0400

    Added links to EN, FR and IT. Completed FR yml except form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'customers/index'
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :customers
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    get 'customers/index'
+    root to: 'pages#home'
+    resources :customers
+  end
 end
