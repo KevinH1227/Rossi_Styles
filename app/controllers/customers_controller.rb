9058be5589b2a78a1f0a9765b88c469d2ff06ea3
Kevin Hookumchand <khookumchand@gmail.com>
2022-03-15 21:45:22 -0400

    added french text and dropdown menu for languages, added flags for languages

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,19 +1,23 @@
 class CustomersController < ApplicationController
   def index
+    @customer = Customer.all
+  end
+
+  def new
     @customer = Customer.new
   end
 
   def create
     @customer = Customer.new(customer_params)
-      if @customer.save
-        cookies[:saved_lead] = true
-        redirect_to root_path, notice: "Saved Successfully!"
-      else
-        redirect_to root_path, notice: "Failed to save!"
-      end
+    if @customer.save
+      cookies[:saved_lead] = true
+      redirect_to root_path, notice: "Saved Successfully!"
+    else
+      redirect_to root_path, notice: "Failed to save!"
+    end
   end
 
-private
+  private
 
   def customer_params
     params.require(:customer).permit(:first_name, :last_name, :email, :phone_number, :message)
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    get 'customers/index'
+    get 'customers/new'
     root to: 'pages#home'
     resources :customers
   end
